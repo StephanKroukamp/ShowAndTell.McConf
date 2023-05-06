214ee1710c68d19d5bc812cf84be1f1da93b6f66
Neil Jansen <neilj@agilebridge.co.za>
2023-05-06 11:30:43 +0200

    changes

---
+++ ShowAndTell.McConf.Infrastructure/Services/DALLEApiClient.cs
@@ -0,0 +1,35 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowAndTell.McConf.Infrastructure.Services
+{
+    public class DALLEApiClient
+    {
+        private readonly RestClient _client;
+
+        public DALLEApiClient(string apiKey)
+        {
+            _client = new RestClient("https://api.openai.com/v1/");
+            _client.AddDefaultHeader("Authorization", $"Bearer {apiKey}");
+        }
+
+        public async Task<Video> GenerateVideo(Script script)
+        {
+            var request = new RestRequest("dalle-mini/video/generate", Method.POST);
+            request.AddJsonBody(new
+            {
+                prompt = script.Text,
+                size = $"{script.Width}x{script.Height}",
+                fps = script.FPS
+            });
+
+            var response = await _client.ExecuteAsync(request);
+            var content = JsonConvert.DeserializeObject<Video>(response.Content);
+            return content;
+        }
+    }
+}
